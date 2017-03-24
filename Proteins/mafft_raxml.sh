317ac0b57ce4b700890220b79b745f88d6c84200
venkat <tallavenkat.vit@gmail.com>
2017-03-24 15:32:02 +0100

    Update mafft_raxml.sh

--- Proteins/mafft_raxml.sh
+++ Proteins/mafft_raxml.sh
@@ -1,4 +1,3 @@
 mafft --thread 4 --threadit 0 --reorder --anysymbol --auto --adjustdirection concat_final_with_pop.fa > new-Rotated.mafft
 
-raxmlHPC-HYBRID-AVX -T 4 -s new-Rotated.mafft -N autoMRE -n concat_conserved_final -f a -m GTRPROTGAMA -N 100 -x 12345 -p 12345 -o Manduca_sexta,Bombyx_mori
-
+raxmlHPC-HYBRID-AVX -T 10 -s concat_with_phoebis.final -N autoMRE -n concat_conserved_final_final -f a -m PROTGAMMAGTR -# 10000 -x 12345 -p 12345 -o Manduca_sexta,Bombyx_mori 
